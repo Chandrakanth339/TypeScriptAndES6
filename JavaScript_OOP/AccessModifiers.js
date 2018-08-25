e9cc839d1502648b1da55f4de4f1e70b8f85dbcc
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-08-26 04:25:45 +0530

    ++ in ,Object.keys(o: object)

--- JavaScript_OOP/AccessModifiers.js
+++ JavaScript_OOP/AccessModifiers.js
@@ -1,5 +1,6 @@
 function Circle(num){
     this.radius = num,
+    
     draw = () =>{
         console.log("Funny");
     }
@@ -11,6 +12,21 @@ console.log("Field accessor using second variable "+circle['radius'] );
 // You may assign the field name accessor to a variable and reuse
 var fieldName = 'radius';
 console.log("Field accessor example using third variable "+circle[fieldName]);
-
-delete circle[fieldName];
+// Delete a property from an Object
+// delete circle[fieldName];
 console.log(circle);
+
+// Loop through key value pairs
+for(let key in circle){
+    if(typeof circle[key] !== 'function')
+        console.log(key);
+}
+
+// to get all property names from a given object - as an array of keys (starting from indx 0)
+const keyz = Object.keys(circle);
+console.log(keyz);
+
+// To check if a property exists in an object
+if('radius' in circle)
+    console.log("Radius property exists in Circle");
+
